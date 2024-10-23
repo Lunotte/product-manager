0b6692d40128762e66d8920285026f2320d2ff07
Lunotte <charly.beaugrand@gmail.com>
2024-10-23 22:20:19 +0200

    Split handle process

---
+++ src/entrypoints/handle-crud.ts
@@ -0,0 +1,88 @@
+import { ipcMain} from 'electron';
+import { Produit } from '../models/Produit';
+import { Categorie } from '../models/Categorie';
+import { Fournisseur } from '../models/Fournisseur';
+import { Unite } from '../models/Unite';
+import db from '../db/database';
+
+export const crudHandlers = () => {
+    ipcMain.handle('get-categories', (): Categorie[] => {
+        return db.getCategories();
+    });
+    
+    ipcMain.handle('add-categorie', (_, nom: string): Categorie[] => {
+        db.addCategory(nom);
+        return db.getCategories();
+    });
+
+    ipcMain.handle('update-categorie', (_, id: number, nom: string): Categorie[] => {
+        db.updateCategory(nom, id);
+        return db.getCategories();
+    });
+    
+    ipcMain.handle('delete-categorie', (_, id: number): Categorie[] => {
+        db.deleteCategory(id);
+        return db.getCategories();
+    });
+
+    ipcMain.handle('get-fournisseurs', (): Fournisseur[] => {
+        return db.getFournisseurs();
+    });
+
+    ipcMain.handle('add-fournisseur', (_, nom: string): Fournisseur[] => {
+        db.addFournisseur(nom);
+        return db.getFournisseurs();
+    });
+
+    ipcMain.handle('update-fournisseur', (_, id: number, nom: string): Fournisseur[] => {
+        db.updateFournisseur(nom, id);
+        return db.getFournisseurs();
+    });
+
+    ipcMain.handle('delete-fournisseur', (_, id: number): Fournisseur[] => {
+        db.deleteFournisseur(id);
+        return db.getFournisseurs();
+    });
+
+    ipcMain.handle('get-unites', (): Unite[] => {
+        return db.getUnites();
+    });
+
+    ipcMain.handle('add-unite', (_, nom: string): Unite[] => {
+        db.addUnite(nom);
+        return db.getUnites();
+    });
+
+    ipcMain.handle('update-unite', (_, id: number, nom: string): Unite[] => {
+        db.updateUnite(nom, id);
+        return db.getUnites();
+    });
+
+    ipcMain.handle('delete-unite', (_, id: number): Unite[] => {
+        db.deleteUnite(id);
+        return db.getUnites();
+    });
+
+    ipcMain.handle('get-produits', (): Produit[] => {
+        return db.getProduits();
+    });
+
+    ipcMain.handle('rechercher-produit', (_, query: string): Produit[] => {
+        return db.rechercherProduit(query);
+    });
+
+    ipcMain.handle('add-produit', (_, produit: Produit): Produit[] => {
+        db.addProduit(produit.nom, produit.prixAchat, produit.taux, produit.prixVente, produit.categorieId, produit.fournisseurId, produit.uniteId);
+        return db.getProduits();
+    });
+
+    ipcMain.handle('update-produit', (_, produit: Produit): Produit[] => {
+        db.updateProduit(produit.id, produit.nom, produit.prixAchat, produit.taux, produit.prixVente, produit.categorieId, produit.fournisseurId, produit.uniteId);
+        return db.getProduits();
+    });
+
+    ipcMain.handle('delete-produit', (_, id: number): Produit[] => {
+        db.deleteProduit(id);
+        return db.getProduits();
+    })
+};
